06c944657a29d3b9d2a54985b36ac905b95774ff
Barney <dogier431@gmail.com>
2024-06-04 00:21:40 +0200

    home page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "standardowy":"#699",
+        "drugi":"#777"
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
