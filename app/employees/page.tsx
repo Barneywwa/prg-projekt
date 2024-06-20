36d53bd18b1d7527540e5dae9258e431cb87011e
Barney <dogier431@gmail.com>
2024-06-20 21:23:53 +0200

    add employees page

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -1,38 +1,25 @@
-import Image from "next/image";
-import ShopImagePath from "@/public/images/shop.png";
-import WarehouseImagePath from "@/public/images/warehouse.png";
-import WorkerImagePath from "@/public/images/worker.png";
+import { DynamicMap } from '@/components/DynamicMap';
+import { EmployeesTable } from '@/components/EmployeesTable';
+import { supabase } from '@/lib/supabase';
 
-export default function EmployeesPage() {
+export const revalidate = 0;
+
+export default async function EmployeesPage() {
+  const { data: warehouses } = await supabase.from('warehouses').select();
+  const { data: shops } = await supabase.from('shops').select();
+  const { data: employees } = await supabase.from('employees').select();
+
   return (
-    <div className="flex flex-col min-h-[90vh] justify-center items-center text-center gap-16">
-      <h1 className="text-8xl font-bold">Choose service</h1>
-      <div className="flex">
-        <div>
-          <div className="w-80 h-80 relative mx-auto">
-            <Image src={ShopImagePath} alt="" fill />
-          </div>
-          <button className="uppercase bg-[#19446B] text-white px-9 py-2 text-3xl rounded-lg hover:bg-[#92a9d0] ">
-            Oddziały sklepów
-          </button>
-        </div>
-        <div>
-          <div className="w-80 h-80 relative mx-auto">
-            <Image src={WorkerImagePath} alt="" fill />
-          </div>
-          <button className="uppercase bg-[#19446B] text-white px-9 py-2 text-3xl rounded-lg hover:bg-[#92a9d0] ">
-            Pracownicy
-          </button>
-        </div>
-        <div>
-          <div className="w-80 h-80 relative mx-auto">
-            <Image src={WarehouseImagePath} alt="" fill />
-          </div>
-          <button className="uppercase bg-[#19446B] text-white px-9 py-2 text-3xl rounded-lg hover:bg-[#92a9d0] ">
-            Oddziały Magazynowe
-          </button>
-        </div>
+    <main className="flex flex-col max-w-7xl justify-center items-center mx-auto py-12 px-8 bg-white">
+      <h1 className="text-3xl font-bold mb-10">Pracownicy</h1>
+      <div className="h-[420px] rounded-md w-full bg-slate-500">
+        <DynamicMap
+          warehouses={warehouses}
+          shops={shops}
+          employees={employees}
+        />
       </div>
-    </div>
+      <EmployeesTable employees={employees} />
+    </main>
   );
 }
