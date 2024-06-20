de6f874b52bcf7022bacb5575049febb138b3b4f
Barney <dogier431@gmail.com>
2024-06-20 21:21:39 +0200

    add about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,12 @@
 export default function AboutPage() {
   return (
-    <div className="flex flex-col min-h-[90vh] justify-center items-center text-center gap-4">
-      <h1 className="text-7xl font-bold">About server</h1>
-      <p className=" text-3xl">
+    <main className="flex flex-col max-w-7xl justify-center items-center mx-auto py-24 px-8 bg-white">
+      <h1 className="text-4xl font-bold mb-10">O serwerze</h1>
+      <p className="max-w-2xl mx-auto text-justify text-lg">
         Tutaj zostanie opisana geneza oraz działanie geoserwera, stworzonego na
         potrzebę uporządkowania danych o pracownikach, sklepach oraz sieciach
         sklepów jak i ich magazynach.
       </p>
-    </div>
+    </main>
   );
 }
